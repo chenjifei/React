8d472371d4eb0eeccae023bacd3ff92d9f0ed17e
chenjifei <chenjifei0324@163.com>
2020-11-20 00:18:14 +0800

    delete code

--- React-Redux-Router-Ts/src/containers/configStore.ts
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { combineReducer } from "./combineReducer";
-import DevTools from "./DevTools";
-const finalCreateStore = compose(
-    applyMiddleware(thunk)
-)(createStore)
-
-export const store = finalCreateStore(combineReducer, DevTools.instrument());
