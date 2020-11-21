6ea13a44e9bf466172c065bb18834ac12425fc6b
chenjifei <chenjifei0324@163.com>
2020-11-21 10:46:16 +0800

    remove code

--- React-Redux-Router-Ajax/src/containers/configStore.ts
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleWare from 'redux-thunk';
-import allReducer from "./ConcatReducer";
-import DevTools from "./DevTools";
-let tempcompose = compose(applyMiddleware(thunkMiddleWare), DevTools.instrument())
-let store = createStore(allReducer, ['use redux in react'], tempcompose);
-
-export default store;
